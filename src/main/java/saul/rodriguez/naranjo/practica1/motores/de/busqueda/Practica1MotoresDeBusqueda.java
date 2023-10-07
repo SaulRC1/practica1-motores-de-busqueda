f69177bea719c9bf23685dbd3591f7e831826aba
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-10-07 02:22:52 +0200

    Input of documents to Apache Solr

--- src/main/java/saul/rodriguez/naranjo/practica1/motores/de/busqueda/Practica1MotoresDeBusqueda.java
+++ src/main/java/saul/rodriguez/naranjo/practica1/motores/de/busqueda/Practica1MotoresDeBusqueda.java
@@ -1,12 +1,36 @@
 package saul.rodriguez.naranjo.practica1.motores.de.busqueda;
 
+import java.io.IOException;
+import java.util.List;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.UpdateResponse;
+import org.apache.solr.common.SolrInputDocument;
+import saul.rodriguez.naranjo.practica1.motores.de.busqueda.indexing.parse.CorpusFormatDocumentParser;
+import saul.rodriguez.naranjo.practica1.motores.de.busqueda.indexing.parse.DocumentParser;
+import saul.rodriguez.naranjo.practica1.motores.de.busqueda.indexing.parse.WrongDocumentExtensionException;
+
 /**
  *
- * @author SaulRC1
+ * @author Saúl Rodríguez Naranjo
  */
 public class Practica1MotoresDeBusqueda {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SolrServerException, IOException, WrongDocumentExtensionException {
+        final SolrClient client = new HttpSolrClient.Builder("http://localhost:8983/solr").build();
+        
+        DocumentParser corpusDocumentParser = new CorpusFormatDocumentParser();
+        
+        List<SolrInputDocument> solrInputDocumentList = corpusDocumentParser.parseDocument("src/main/resources/cisi/CISI.ALL");
+        
+        for (SolrInputDocument solrInputDocument : solrInputDocumentList)
+        {
+            System.out.println("Adding " + solrInputDocument.getFieldValue(CorpusFormatDocumentParser.SOLR_INPUT_DOCUMENT_IDENTIFIER_KEY));
+            
+            final UpdateResponse updateResponse = client.add("micoleccion", solrInputDocument);
+            
+            client.commit("micoleccion");
+        }
     }
 }
